a4536556de6cf5f024f7a592c1fc59dffe66aecf
Lucas Jähnel <jahnelemanuel@gmail.com>
2022-10-10 12:24:42 -0300

    Updates

---
+++ precache-manifest.dff5c829feb4cc0d3f186271e620fcb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "051b528a3c6216f355e7",
+    "url": "/ReactJS-Coderhouse/static/css/main.94bb169e.chunk.css"
+  },
+  {
+    "revision": "051b528a3c6216f355e7",
+    "url": "/ReactJS-Coderhouse/static/js/main.051b528a.chunk.js"
+  },
+  {
+    "revision": "246a0fec9f2840851a0f",
+    "url": "/ReactJS-Coderhouse/static/js/1.246a0fec.chunk.js"
+  },
+  {
+    "revision": "97f6430f60758e572ec3",
+    "url": "/ReactJS-Coderhouse/static/js/runtime~main.97f6430f.js"
+  },
+  {
+    "revision": "470f0c912cb50764cb4e8286729c8ca8",
+    "url": "/ReactJS-Coderhouse/index.html"
+  }
+];
