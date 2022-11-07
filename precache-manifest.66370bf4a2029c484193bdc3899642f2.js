5d8fc88987cdcbafadabeb213bf77d7f50fe1bde
Lucas Jähnel <jahnelemanuel@gmail.com>
2022-11-07 19:08:56 -0300

    Updates

--- precache-manifest.66370bf4a2029c484193bdc3899642f2.js
+++ precache-manifest.66370bf4a2029c484193bdc3899642f2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "388a77ccc967df6be28c",
+    "revision": "05119931b8f0082fe547",
     "url": "/ReactJS-Coderhouse/static/css/main.36091754.chunk.css"
   },
   {
-    "revision": "388a77ccc967df6be28c",
-    "url": "/ReactJS-Coderhouse/static/js/main.388a77cc.chunk.js"
+    "revision": "05119931b8f0082fe547",
+    "url": "/ReactJS-Coderhouse/static/js/main.05119931.chunk.js"
   },
   {
     "revision": "c9002264886cf13d5546",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ReactJS-Coderhouse/static/js/runtime~main.97f6430f.js"
   },
   {
-    "revision": "deadc916fe66fe9590358c3c0aaa9762",
+    "revision": "cf40b3e0931bc597835fb38c53523bd4",
     "url": "/ReactJS-Coderhouse/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactJS-Coderhouse/precache-manifest.c4aa5208ef64523be62360b38355ea84.js"
+  "/ReactJS-Coderhouse/precache-manifest.66370bf4a2029c484193bdc3899642f2.js"
 );
 
 workbox.clientsClaim();
